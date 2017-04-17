54838c5e65e88f1d700966116fb938657f791063
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2017-04-17 18:27:06 +0530

    Helloworld program

---
+++ C-Programming/Basic-Programming/helloworld.c
@@ -0,0 +1,15 @@
+/**
+ * First program which welcomes user to the programming world.
+ */
+
+/** Header file inclusion which contains declarations 
+    for printf, scanf (i/o function prototype) 
+*/
+#include <stdio.h>
+
+/* Main function entry*/
+int main()
+{
+	printf("Hello world, welcome to programming world !!\n");
+	return 0;
+}
